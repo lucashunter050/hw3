0026ca4b02c929f4221d6c373ed74edf913ebbee
Lucas Hunter <lphunter@usc.edu>
2022-10-17 19:51:57 -0700

    part 6 completed commit

---
+++ logicsim/event.h
@@ -0,0 +1,26 @@
+#ifndef EVENT_H
+#define EVENT_H
+#include "wire.h"
+
+struct Event
+{
+    uint64_t time;
+    Wire* wire;
+    char state;
+};
+
+typedef struct EventLess {
+        //write the operator() required to make this a functor that compares Events by time
+    bool operator() (Event* first, Event* second) {
+        bool firstHasPriority = false;
+        if (first->time < second->time) {
+            // first has priority
+            firstHasPriority = true;
+        } 
+
+        return firstHasPriority;
+        
+    } 
+} EventLess;
+	
+#endif
