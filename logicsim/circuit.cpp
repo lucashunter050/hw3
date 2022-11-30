6f1ce368a716b3ab1166b305c2b5dfe881309783
Lucas Hunter <lphunter@usc.edu>
2022-11-30 04:42:19 +0000

    final regrade changes for working hw3

--- logicsim/circuit.cpp
+++ logicsim/circuit.cpp
@@ -116,7 +116,7 @@ bool Circuit::parse(const char* fname)
                     std::string s_in1;
                     getline(ss, s_in1, ',');
                     std::string s_output;
-                    getline(ss, s_output, '\n');
+                    getline(ss, s_output, ',');
                     m_gates.push_back(new NotGate(m_wires[stoi(s_in1)], m_wires[stoi(s_output)]));
                 }
             }
